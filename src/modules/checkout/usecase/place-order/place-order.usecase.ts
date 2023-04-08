89c73377551e28b239ee5e12246b647787ceeaf2
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-04-08 13:46:17 -0300

    feat: add check stock methods

--- src/modules/checkout/usecase/place-order/place-order.usecase.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.ts
@@ -1,12 +1,19 @@
 import UseCaseInterface from "../../../@shared/usecase/use-case.interface";
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from "./place-order.dto";
 import ClientAdmFacadeInterface from "../../../client-adm/facade/client-adm.facade.interface";
+import ProductAdmFacade from "../../../product-adm/facade/product-adm.facade";
+import ProductAdmFacadeInterface from "../../../product-adm/facade/product-adm.facade.interface";
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
   private clientFacade: ClientAdmFacadeInterface;
+  private productFacade: ProductAdmFacadeInterface;
 
-  constructor(clientFacade: ClientAdmFacadeInterface) {
+  constructor(
+    clientFacade: ClientAdmFacadeInterface,
+    productFacade: ProductAdmFacadeInterface
+  ) {
     this.clientFacade = clientFacade;
+    this.productFacade = productFacade;
   }
 
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
@@ -21,6 +28,19 @@ export default class PlaceOrderUseCase implements UseCaseInterface {
   private async validateProducts(input: PlaceOrderInputDto): Promise<void> {
     if (input.products.length === 0) {
       throw new Error("No products selected");
+    }
+
+    await this.checkStock(input.products);
+  }
+
+  private async checkStock(products: Array<any>): Promise<void> {
+    for (const p of products) {
+      const product = await this.productFacade.checkStock({
+        productId: p.productId,
+      });
+      if (product.stock <= 0) {
+        throw new Error(`Product ${p.productId} is not available in stock`);
+      }
     }
   }
 }
