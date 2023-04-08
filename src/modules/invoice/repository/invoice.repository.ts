10f4c38b928ba73f94ebd2bf4e8f9fdc84cac425
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-04-07 22:13:42 -0300

    feat: add invoice repository

---
+++ src/modules/invoice/repository/invoice.repository.ts
@@ -0,0 +1,73 @@
+import { InternetModule } from "@faker-js/faker";
+import Id from "../../@shared/domain/value-object/id.value-object";
+import Invoice from "../domain/invoice.entity";
+import Product from "../domain/product.entity";
+import InvoiceGateway from "../gateway/invoice.gateway";
+import Address from "../valueobject/address.valueobject";
+import { InvoiceModel } from "./invoice.model";
+import { ProductModel } from "./product.model";
+
+export default class InvoiceRepository implements InvoiceGateway {
+  async generate(invoice: Invoice): Promise<void> {
+    await InvoiceModel.create(
+      {
+        id: invoice.id.id,
+        name: invoice.name,
+        document: invoice.document,
+        street: invoice.address.street,
+        number: invoice.address.number,
+        complement: invoice.address.complement,
+        city: invoice.address.city,
+        state: invoice.address.state,
+        zipCode: invoice.address.zipCode,
+        items: invoice.items.map((item) => ({
+          id: item.id.id,
+          name: item.name,
+          price: item.price,
+        })),
+        createdAt: invoice.createdAt,
+        updatedAt: invoice.updatedAt,
+      },
+      {
+        include: [{ model: ProductModel }],
+      }
+    );
+  }
+
+  async find(id: string): Promise<Invoice> {
+    const invoice = await InvoiceModel.findOne({
+      where: { id },
+      include: [{ model: ProductModel }],
+    });
+
+    if (!invoice) {
+      throw new Error(`Invoice with id ${id} not found`);
+    }
+
+    return new Invoice({
+      id: new Id(invoice.id),
+      name: invoice.name,
+      document: invoice.document,
+      address: new Address({
+        street: invoice.street,
+        number: invoice.number,
+        complement: invoice.complement,
+        city: invoice.city,
+        state: invoice.state,
+        zipCode: invoice.zipCode,
+      }),
+      items: invoice.items.map(
+        (item) =>
+          new Product({
+            id: new Id(item.id),
+            name: item.name,
+            price: item.price,
+            createdAt: item.createdAt,
+            updatedAt: item.updatedAt,
+          })
+      ),
+      createdAt: invoice.createdAt,
+      updatedAt: invoice.updatedAt,
+    });
+  }
+}
